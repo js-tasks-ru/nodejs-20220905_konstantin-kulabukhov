9f1bd3b547464ce0e1faaac0806762156a8980c8
Konstantin Kulabukhov <konstantin.kulabukhov@gmail.com>
2022-09-25 22:03:08 +0300

    create delete file server

--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,10 +1,10 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const {unlink} = require('fs/promises');
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+server.on('request', async (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
@@ -12,6 +12,28 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
+
+      const pathSize = pathname.split('/').filter(Boolean).length;
+
+      if (pathSize > 1) {
+        res.statusCode = 400;
+        res.end();
+        return;
+      }
+
+      try {
+        await unlink(filepath);
+        res.end();
+      } catch (error) {
+        if (error.code === 'ENOENT') {
+          res.statusCode = 404;
+        } else {
+          res.statusCode = 500;
+        }
+
+        res.end();
+        return;
+      }
 
       break;
 
