297815fc5c6812afd58909a21d4623b7ef5758dc
Konstantin Kulabukhov <konstantin.kulabukhov@gmail.com>
2022-10-16 19:24:51 +0300

    add text search

--- 06-mongodb-mongoose/03-search/controllers/products.js
+++ 06-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,14 @@
+const Product = require('../models/Product');
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const {query} = ctx.query;
+
+  if (!query) {
+    ctx.body = {products: []};
+    return;
+  }
+
+  const products = await Product.find({$text: {$search: query}});
+
+  ctx.body = {products};
 };
